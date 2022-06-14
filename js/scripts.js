9ba0de1884d36d1488373ba933ab89c179722785
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-06-13 21:58:21 -0700

    adds rought sketch of functionality and form branching

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//  if animal = emperor penguin do penguin di 
+else if frog = do the frog stuff
+else if two toed sloth stuff
+else 'we don't have information on that Animal
+
+window.onload = function() {
+  animal.style.display = "none";
+}
+
+const form = document.querySelector("form");
+form.onsubmit = function(event) {
+  
+}
+
+Form.apply.onsubmit = function(event) {
+
+}
